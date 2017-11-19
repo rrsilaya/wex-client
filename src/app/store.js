641fd223b4eac7f7aef71a1609b3dada1506ef1b
rrsilaya <rrsilaya@up.edu.ph>
2017-11-19 15:47:31 +0800

    Fixed mistype

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { middlware as reduxpack } from 'redux-pack';
+import { middleware as reduxpack } from 'redux-pack';
 
 import reducers from '../features';
 
