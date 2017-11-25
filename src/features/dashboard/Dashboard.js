bdaa9619a06f17f8f9a7ebaf78d4e171f229d440
mikenmo <mdmojar@up.edu.ph>
2017-11-25 10:55:26 +0800

    Edit Dashboard

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
 
+import Header from 'grommet/components/Header';
+import Button from 'grommet/components/Button';
+import Select from 'grommet/components/Select';
+import Table from 'grommet/components/Table';
+import TableRow from 'grommet/components/TableRow';
+
+import AddIcon from 'grommet/components/icons/base/Add';
+import './dash.css';
 class Dashboard extends Component {
+  asd = () => {
+    console.log('asd');
+  };
   render() {
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="center">
+        <Header size={'small'}>
+          <Select
+            placeHolder="Choose Category"
+            options={['Movies', 'Memes', 'TV Series', 'Anime']}
+            onSearch={false}
+            value={undefined}
+          />
+          <Button icon={<AddIcon />} secondary={true} onClick={this.asd} />
+        </Header>
+        <br />
+
+        <Table>
+          <thead>
+            <tr>
+              <th>Question</th>
+              <th>Answer</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TableRow>
+              <td>Somethingsienasdasd</td>
+              <td>A</td>
+            </TableRow>
+            <TableRow>
+              <td>asdasdsd</td>
+              <td>C</td>
+            </TableRow>
+            <TableRow>
+              <td>ADWESDASD</td>
+              <td>D</td>
+            </TableRow>
+            <TableRow>
+              <td>qweascasd</td>
+              <td>D</td>
+            </TableRow>
+          </tbody>
+        </Table>
       </div>
     );
   }
--- src/features/session/Loader.js
+++ src/features/session/Loader.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Spinning from 'grommet/components/icons/Spinning';
-
+import Box from 'grommet/components/Box';
 import './style.css';
-
 class Loader extends Component {
   render() {
     return (
       <div className="full">
-        <Spinning size="large" />
+        <Box>
+          <Spinning size="large" />
+        </Box>
       </div>
     );
   }
