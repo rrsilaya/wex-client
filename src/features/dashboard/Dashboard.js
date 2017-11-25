ac1814e18df57e3afc4e89a7ccfc88bfe62a8611
mikenmo <mdmojar@up.edu.ph>
2017-11-25 15:03:04 +0800

    Edit dashboard, Add dashboard css

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 
-import Header from 'grommet/components/Header';
-import Button from 'grommet/components/Button';
-import Select from 'grommet/components/Select';
-import Table from 'grommet/components/Table';
-import TableRow from 'grommet/components/TableRow';
+import Accordion from 'grommet/components/Accordion';
+import AccordionPanel from 'grommet/components/AccordionPanel';
+import Heading from 'grommet/components/Heading';
 
 import AddIcon from 'grommet/components/icons/base/Add';
+
 import './dash.css';
 class Dashboard extends Component {
   asd = () => {
@@ -14,44 +13,170 @@ class Dashboard extends Component {
   };
   render() {
     return (
-      <div className="center">
-        <Header size={'small'}>
-          <Select
-            placeHolder="Choose Category"
-            options={['Movies', 'Memes', 'TV Series', 'Anime']}
-            onSearch={false}
-            value={undefined}
-          />
-          <Button icon={<AddIcon />} secondary={true} onClick={this.asd} />
-        </Header>
-        <br />
-
-        <Table>
-          <thead>
-            <tr>
-              <th>Question</th>
-              <th>Answer</th>
-            </tr>
-          </thead>
-          <tbody>
-            <TableRow>
-              <td>Somethingsienasdasd</td>
-              <td>A</td>
-            </TableRow>
-            <TableRow>
-              <td>asdasdsd</td>
-              <td>C</td>
-            </TableRow>
-            <TableRow>
-              <td>ADWESDASD</td>
-              <td>D</td>
-            </TableRow>
-            <TableRow>
-              <td>qweascasd</td>
-              <td>D</td>
-            </TableRow>
-          </tbody>
-        </Table>
+      <div className="margin">
+        <Heading tag={'h2'}> Questions </Heading>
+        <Accordion openMulti={true}>
+          <AccordionPanel heading="TV Series" pad={'medium'}>
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+          </AccordionPanel>
+          <AccordionPanel heading="Memes" pad={'medium'}>
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+          </AccordionPanel>
+          <AccordionPanel heading="Movies" pad={'medium'}>
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+          </AccordionPanel>
+          <AccordionPanel heading="Anime" pad={'medium'}>
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+          </AccordionPanel>
+          <AccordionPanel heading="Category" pad={'medium'}>
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <br />
+          </AccordionPanel>
+        </Accordion>
       </div>
     );
   }
