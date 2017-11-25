49d92b8bdd08bd2298f2d0de4142da63ed7cdb01
mikenmo <mdmojar@up.edu.ph>
2017-11-25 17:32:26 +0800

    Add dashboard buttons

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 import Accordion from 'grommet/components/Accordion';
 import AccordionPanel from 'grommet/components/AccordionPanel';
 import Heading from 'grommet/components/Heading';
+import Button from 'grommet/components/Button';
 
 import AddIcon from 'grommet/components/icons/base/Add';
+import EditIcon from 'grommet/components/icons/base/Edit';
+import TrashIcon from 'grommet/components/icons/base/Trash';
 
 import './dash.css';
 class Dashboard extends Component {
@@ -16,100 +19,108 @@ class Dashboard extends Component {
       <div className="margin">
         <Heading tag={'h2'}> Questions </Heading>
         <Accordion openMulti={true}>
-          <AccordionPanel heading="TV Series" pad={'medium'}>
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-          </AccordionPanel>
-          <AccordionPanel heading="Memes" pad={'medium'}>
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-            <Heading tag={'h3'}> Difficulty</Heading>
-            <div>
-              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
-            </div>
-            <div>Answer: </div>
-            <br />
-          </AccordionPanel>
           <AccordionPanel heading="Movies" pad={'medium'}>
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
+            <br />
+          </AccordionPanel>
+          <AccordionPanel heading="TV Series" pad={'medium'}>
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
+            <br />
+            <Heading tag={'h3'}> Difficulty</Heading>
+            <div>
+              QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
+            </div>
+            <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
           </AccordionPanel>
           <AccordionPanel heading="Anime" pad={'medium'}>
@@ -118,65 +129,114 @@ class Dashboard extends Component {
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
           </AccordionPanel>
-          <AccordionPanel heading="Category" pad={'medium'}>
+          <AccordionPanel heading="Memes" pad={'medium'}>
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
             <Heading tag={'h3'}> Difficulty</Heading>
             <div>
               QuestionQuestionQuestionQuestionQuestionQuestionQuestionQuestion?
             </div>
             <div>Answer: </div>
+            <div className="buttons">
+              <Button icon={<EditIcon size={'small'} />} onClick={this.asd} />
+              <Button icon={<TrashIcon size={'small'} />} onClick={this.asd} />
+            </div>
             <br />
           </AccordionPanel>
         </Accordion>
+        <br />
+        <div className="buttons">
+          <Button
+            icon={<AddIcon />}
+            label="Add Question"
+            primary={true}
+            onClick={this.asd}
+          />
+        </div>
       </div>
     );
   }
